6ed4b0c0bf8b7753efaa462f8df44e7d7a2f7a09
christian-cahig <67888758+christian-cahig@users.noreply.github.com>
2022-10-10 20:20:55 +0800

    Update DOI in help texts of installation and experiment functions

--- diff-apf2/calcMetric.m
+++ diff-apf2/calcMetric.m
@@ -6,7 +6,7 @@ function [out] = calcMetric(pdata, Vm, Va, Ps, NameVals)
 %
 %   A Computational Approach to Anticipating Supply Injections and Bus Voltages in Steady-State Power System Analysis
 %   Copyright (C) 2022 - present, Christian Cahig
-%   https://doi.org/10.5281/zenodo.7077324
+%   https://doi.org/10.5281/zenodo.7180586
 % 
 %   This file is part of a repository under the CC-BY-4.0 License.
 %   See `LICENSE` file in the source directory for details.
